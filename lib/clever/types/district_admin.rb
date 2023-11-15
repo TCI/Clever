a868da017db864aaa5b11c08b22a91ab23c15c2e
Brent Wishart <bwishart@teachtci.com>
2023-11-15 15:00:24 -0800

    add options arg

--- lib/clever/types/district_admin.rb
+++ lib/clever/types/district_admin.rb
@@ -3,7 +3,7 @@
 module Clever
   module Types
     class DistrictAdmin < Teacher
-      def initialize(attributes = {}, *, client: nil)
+      def initialize(attributes = {}, *, options)
         @district_username = attributes.dig('roles', 'district_admin', 'credentials', 'district_username')
         @email             = attributes['email']
         @first_name        = attributes['name']['first']
@@ -11,7 +11,7 @@ module Clever
         @provider          = 'clever'
         @sis_id            = attributes.dig('roles', 'district_admin', 'sis_id')
         @uid               = attributes['id']
-        @username          = username(client)
+        @username          = username(options[:client])
         @role              = 'admin'
       end
     end
