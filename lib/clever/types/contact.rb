5fe29f8145c1b0066028ee8527d455e6d36fd48d
Hakim Aryan <hakim.aryan@cpoms.co.uk>
2025-07-09 13:00:08 +0100

    Migrate contacts API endpoint to use v3 instead of v2

--- lib/clever/types/contact.rb
+++ lib/clever/types/contact.rb
@@ -4,20 +4,24 @@ module Clever
   module Types
     class Contact < Base
       attr_reader :uid, :sis_id, :name, :students, :school, :district,
-        :phone_type, :phone, :email, :type, :relationship
+        :phone_type, :phone, :email
 
       def initialize(attributes = {}, *)
         @uid = attributes['id']
-        @sis_id = attributes['sis_id']
-        @name = attributes['name']
-        @students = attributes['students']
+        @sis_id = dig_contact_role(attributes, "sis_id")
+        @name = attributes.dig('name', 'last')
+        @students = dig_contact_role(attributes, "student_relationships")
         @school = attributes['school']
         @district = attributes['district']
-        @phone_type = attributes['phone_type']
-        @phone = attributes['phone']
+        @phone_type = dig_contact_role(attributes, "phone_type")
+        @phone = dig_contact_role(attributes, "phone")
         @email = attributes['email']
-        @type = attributes['type']
-        @relationship = attributes['relationship']
+      end
+
+      private
+
+      def dig_contact_role(attributes, key)
+        attributes.dig('roles', 'contact', key)
       end
     end
   end
--- lib/clever/version.rb
+++ lib/clever/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Clever
-  VERSION = '3.2.6'
+  VERSION = '3.3.0'
 end
